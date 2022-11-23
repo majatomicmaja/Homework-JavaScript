9d088828fa00d5416d91736e3bae36b2fe35fbd8
majatomicmaja <118531291+majatomicmaja@users.noreply.github.com>
2022-11-23 16:54:59 +0100

    Add files via upload
    Conditial Braching

---
+++ script.js
@@ -0,0 +1,11 @@
+// 5.Give user the info!
+// •	Write a javascript program that will ask user 3 questions in order
+// •	What is your age?
+// •	What is your name?
+// •	what do you do for a living?
+// •	After the user has given all the answers, alert the user that Username is {name answer from your user} age is {age answer from user} and he is doing {work answer from user)
+
+let age = prompt('What is your age?');
+let userName = prompt('What is your name');
+let job = prompt('What do you do for a living?');
+alert(`Username is ${userName}, age is ${age}, and you are doing ${job} for a living.`);
