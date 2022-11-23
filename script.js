f5cc72fb36eef443b6c710685d014dbce56afc76
majatomicmaja <118531291+majatomicmaja@users.noreply.github.com>
2022-11-23 17:00:40 +0100

    Add files via upload
    Conditional Braching

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+// 1.Using if..else, write the code which gets a number via prompt and then shows in alert: 
+// •	1, if the value is greater than zero, 
+// •	-1, if less than zero, 
+// •	0, if equals zero. 
+// In this task we assume that the input is always a number. 
+
+let value = prompt("Write down a number here");
+
+if(value > 0) {
+    alert(1);
+} else if(value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+// 3 Rewrite a code using tenerary operators 
+// let result; 
+// let a = 1; 
+// let b = 2; 
+ 
+// if (a + b < 5) { 
+//   result = 'Below'; 
+// } else { 
+//   result = 'Over'; 
+// } 
+
+let results;
+let a = 1;
+let b = 2;
+
+a + b < 5 ? results = 'Below' : results = 'Over';
+console.log(results);
+
+// 4. Want to find out how old you'll be? Calculate it!
+// •	Store your birth year in a variable.
+// •	Store a future year in a variable example 2050
+// •	Calculate your 2 possible ages for that year based on the stored values.
+// Output them to the screen like so: "I will be either NN or NN in YYYY", substituting the values.
+
+const birthYear = 1997;
+const futureYear = 2050;
+const result = futureYear - birthYear;
+alert(`I will be ${result} in ${futureYear}`);
+const secondResult = birthYear - futureYear;
+alert(`I will be ${secondResult}`)
+
 // 5.Give user the info!
 // •	Write a javascript program that will ask user 3 questions in order
 // •	What is your age?
